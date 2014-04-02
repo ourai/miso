ece6c0f6d0ba3acd7e27d75c15e2075301b6f278
Ourai Lin <ourairyu@hotmail.com>
2014-04-02 18:46:09 +0800

    A: source code

---
+++ src/js/miso.js
@@ -0,0 +1,60 @@
+"use strict";
+var LIB_CONFIG, storage, toString, _C, _H;
+
+LIB_CONFIG = {
+  name: "@NAME",
+  version: "@VERSION"
+};
+
+_H = {};
+
+toString = Object.prototype.toString;
+
+storage = {
+  types: {}
+};
+
+_C = (function() {
+  function _C() {}
+
+  _C.prototype.mixin = function() {
+    var args, copy, i, length, name, opts, target, _ref;
+    args = arguments;
+    length = args.length;
+    target = (_ref = args[0]) != null ? _ref : {};
+    i = 1;
+    if (length === 1) {
+      target = this;
+      i--;
+    }
+    while (i < length) {
+      opts = args[i];
+      if (typeof opts === "object") {
+        for (name in opts) {
+          copy = opts[name];
+          if (copy === target) {
+            continue;
+          }
+          if (copy !== void 0) {
+            target[name] = copy;
+          }
+        }
+      }
+      i++;
+    }
+    return target;
+  };
+
+  _C.prototype.type = function(object) {
+    if (object == null) {
+      return String(object);
+    } else {
+      return storage.types[toString.call(object)] || "object";
+    }
+  };
+
+  return _C;
+
+})();
+
+window[LIB_CONFIG.name] = _H;
